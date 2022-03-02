7369337bff987efff894360d40c3832d1370b464
Matheus Oliveira <97600307+MatheusOliveira-1@users.noreply.github.com>
2022-03-02 19:45:57 -0300

    desafio03 solucionado usando 'int(' antes do input

--- scripts_idle/desafios/desafio03.py
+++ scripts_idle/desafios/desafio03.py
@@ -1,5 +1,5 @@
 print('============== DESAFIO 03 ===========')
-num1 = input('Primeiro número: ')
-num2 = input('Segundo número: ')
-soma = num1+num2
+num1 = int(input('Primeiro número: '))
+num2 = int(input('Segundo número: '))
+soma = (num1+num2)
 print('Soma:', soma)
