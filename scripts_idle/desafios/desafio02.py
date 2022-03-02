6028cffe204f77e82688fc5364eaa69295cac4ed
Matheus Oliveira <97600307+MatheusOliveira-1@users.noreply.github.com>
2022-03-02 19:36:45 -0300

    realizada alteracao estetica no desafio01, e finalizado o desafio02

---
+++ scripts_idle/desafios/desafio02.py
@@ -0,0 +1,6 @@
+print('============ DESAFIO 01 ============')
+print('Informe sua data de nascimento:')
+dia = input('Dia = ')
+mes = input('Mês = ')
+ano = input('Ano = ')
+print('Você nasceu no dia ' +dia+ ' de ' +mes+ ' de ' +ano+ '. Correto?')
